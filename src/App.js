a2eaad006f83e72fa4084770c3fe886448a31c29
wiranley <wiranley@outlook.com>
2023-03-28 20:35:46 -0300

    Consumindo states do jogo, Evento de inserção de letra e Processando letras válidas e inválidas

--- src/App.js
+++ src/App.js
@@ -25,22 +25,29 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   const pickWordAndCategory = () => {
     //pick a randon category
     const categories = Object.keys(words);
-    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
-    console.log(category)
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
 
     //pick a randon word
-    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
     console.log(word);
-    return { word, category }
+    return { word, category };
   };
 
   //starts the secret word game
   const startGame = () => {
     //pick word and pick category
-    const {word, category} = pickWordAndCategory();
+    const { word, category } = pickWordAndCategory();
 
     //create an array of letters
     let wordLetters = word.split("");
@@ -52,16 +59,40 @@ function App() {
     //fill states
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   };
 
   // process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
-  };
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
 
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    // push guessed letter or remove a chance
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        letter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+  console.log(guessedLetters)
+  console.log(wrongLetters)
   //restarts the game
   const retry = () => {
     setGameStage(stages[0].name);
@@ -70,7 +101,18 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
